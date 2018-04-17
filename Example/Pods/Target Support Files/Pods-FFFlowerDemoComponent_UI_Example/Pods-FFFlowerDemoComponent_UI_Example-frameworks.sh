5620cef1c7879391bae53b643615f4c59fa2d6d7
lanbiao <lanbiao@fengnian.cn>
2018-04-17 10:29:17 +0800

    flower_UI组件1.0.0版本发布

--- Example/Pods/Target Support Files/Pods-FFFlowerDemoComponent_UI_Example/Pods-FFFlowerDemoComponent_UI_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-FFFlowerDemoComponent_UI_Example/Pods-FFFlowerDemoComponent_UI_Example-frameworks.sh
@@ -137,14 +137,12 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/FFFlowerDemoComponent_Config/FFFlowerDemoComponent_Config.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FFFlowerDemoComponent_Tools/FFFlowerDemoComponent_Tools.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FFFlowerDemoComponent_UI/FFFlowerDemoComponent_UI.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/FMDB/FMDB.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MBProgressHUD/MBProgressHUD.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/FFFlowerDemoComponent_Config/FFFlowerDemoComponent_Config.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FFFlowerDemoComponent_Tools/FFFlowerDemoComponent_Tools.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FFFlowerDemoComponent_UI/FFFlowerDemoComponent_UI.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/FMDB/FMDB.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MBProgressHUD/MBProgressHUD.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
--- FFFlowerDemoComponent_UI.podspec
+++ FFFlowerDemoComponent_UI.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'FFFlowerDemoComponent_UI'
-  s.version          = '1'
+  s.version          = '1.0.1'
   s.summary          = 'Flower的基础UI组件模块'
 
 # This description is used to generate tags and improve search results.
